a8a5457a2254b34b3cfc78d25b58fe8bf8a47dd7
rhonorato <rhonorato@apple.com>
2025-06-02 19:12:36 -0300

    add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { Counter } from "components/Counter";
+import { Route, Routes } from "react-router";
 
 function App() {
   return (
-    <>
-      <Counter />
-    </>
+    <Routes>
+      <Route path="/counter" element={<Counter />} />
+      <Route index element={<h1>home</h1>} />
+      <Route path="*" element={<h1>not found</h1>} />
+    </Routes>
   );
 }
 
